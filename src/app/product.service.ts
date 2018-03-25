1e74b2b11ebe26128b041929bbac739ebfc36cf6
kumar chandraketu <kumarchandraketu@yahoo.com>
2018-03-25 00:47:58 -0400

    updated Observable library

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
-import 'rxjs/Observable';
-import {Observable} from 'rxjs/Rx';
+//import 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
